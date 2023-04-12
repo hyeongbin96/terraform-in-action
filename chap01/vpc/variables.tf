95870cf462b8d48b07ea630e5c701f6a4a85990e
hyeongbin <gudqls1539@gmail.com>
2023-04-12 23:52:57 +0900

    add vpc

--- chap01/vpc/variables.tf
+++ chap01/vpc/variables.tf
@@ -1,7 +1,28 @@
-variable "aws_vpc" {
+variable "tags" {
     type = string
+    default = "hist-hb"
+    description = "Additinal your company and name tags"
 }
 
-variable "default_name" {
+variable "aws_az" {
+    type = list
+    default = ["ap-northeast-2a", "ap-northeast-2c"]  
+}
+
+variable "vpc_cidr" {
     type = string
+    default = "172.0.0.0/16"
+    description = "aws vpc cidr"
+}
+
+variable "public_subnet" {
+    type = list
+    default = ["172.0.10.0", "172.0.20.0"]
+    description = "aws public subnet cidr"  
+}
+
+variable "private_subnet" {
+    type = list
+    default = ["172.0.30.0", "172.0.40.0"]
+    description = "aws private subnet cidr" 
 }
--- chap01/vpc/vpc.tf
+++ chap01/vpc/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
-    cidr_block = var.aws_vpc
+    cidr_block = var.vpc_cidr
 
     tags = {
-      Name = "${var.default_name}-vpc"
+      Name = "${var.tags}-vpc"
     }
 }
