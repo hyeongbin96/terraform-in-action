7d5453b1ef306f7cee2c5cdd49c3067393263b0b
hyeongbihn <gudqls1539@gmail.com>
2023-04-12 12:56:51 +0900

    backend config to provider

--- chap01/vpc/provider.tf
+++ chap01/vpc/provider.tf
@@ -3,3 +3,14 @@ provider "aws" {
    shared_credentials_files = [ "~/.aws/credentials" ]
    profile = "histdc-dev-terraform"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-hb-backend"
+    key    = "chap01-vpc.tfstate"
+    region = "ap-northeast-2"
+    profile = "histdc-dev-terraform"
+    # dynamodb_table = "terraform-locks"
+    # encrypt        = true
+  }
+}
